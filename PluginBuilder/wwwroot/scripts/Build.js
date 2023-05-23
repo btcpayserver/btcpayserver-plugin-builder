5b2da93fa0f3dfef1780618d95aa519d99c31e14
Dennis Reimann <mail@dennisreimann.de>
2023-05-23 16:25:23 +0200

    Streamline build page

--- PluginBuilder/wwwroot/scripts/Build.js
+++ PluginBuilder/wwwroot/scripts/Build.js
@@ -1,17 +1,3 @@
-var triggerEl = document.querySelector('#artifacts-tabs li:first-child button');
-bootstrap.Tab.getOrCreateInstance(triggerEl).show();
-hljs.highlightAll();
-
-var term = new Terminal({
-    cursorBlink : false,
-    disableStdin: true,
-    allowTransparency : false,
-    screenReaderMode: true,
-    rows: 300
-});
-
-var logsElem = document.getElementById('terminal');
-var txt = logsElem.innerText;
-logsElem.innerText = "";
-term.open(logsElem);
-term.write(txt);
+(function() {
+    window.hljs.highlightAll();
+})();
