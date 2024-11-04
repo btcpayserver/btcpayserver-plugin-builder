0fb7f906561df3af592a780a07e8ec0f411b04d4
Chukwuleta Tobechi <47084273+TChukwuleta@users.noreply.github.com>
2024-11-04 09:57:33 +0100

    update to latest master

--- PluginBuilder/wwwroot/scripts/template-editor.js
+++ PluginBuilder/wwwroot/scripts/template-editor.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     new TomSelect('#Tags', {
-        create: true, // Allows new tags to be created
+        create: true,
         delimiter: ',',
-        persist: false, // Prevents duplicates
-        maxItems: null // Allows multiple selections
+        persist: false,
+        maxItems: null
     });
 });
