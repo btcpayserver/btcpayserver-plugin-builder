5c1282d4fc788dc71f0f78e18cf939a9cffc921a
Chukwuleta Tobechi <47084273+TChukwuleta@users.noreply.github.com>
2025-05-05 18:03:18 +0100

    remove comment

--- PluginBuilder/ViewModels/Account/PgpKeyViewModel.cs
+++ PluginBuilder/ViewModels/Account/PgpKeyViewModel.cs
@@ -8,15 +8,6 @@ public class PgpKeyViewModel
     public string KeyId { get; set; }
     public string Subkeys { get; set; }
     public DateTime? AddedDate { get; set; }
-
-    /*public string Id { get; set; }
-    public string UserId { get; set; }
-    public string Title { get; set; }
-    public string KeyId { get; set; }
-    public string Fingerprint { get; set; }
-    public string PublicKey { get; set; }
-    public string EmailAddress { get; set; }
-    public DateTime CreatedAt { get; set; }*/
 }
 
 public class AddPgpKeyViewModel
