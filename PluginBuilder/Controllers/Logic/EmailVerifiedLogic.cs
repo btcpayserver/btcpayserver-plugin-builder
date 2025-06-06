60bb8b7224a3cbb000327d8fcb9812c0f6ff5a68
rockstardev <5191402+rockstardev@users.noreply.github.com>
2025-06-06 22:23:30 +0200

    Refactoring EmailVerifiedLogic

--- PluginBuilder/Controllers/Logic/EmailVerifiedLogic.cs
+++ PluginBuilder/Controllers/Logic/EmailVerifiedLogic.cs
@@ -10,14 +10,19 @@ public class EmailVerifiedLogic(
     UserManager<IdentityUser> userManager,
     EmailService emailService)
 {
-    public async Task<bool> IsEmailVerified(NpgsqlConnection conn, ClaimsPrincipal claimsPrincipal)
+    public async Task<bool> IsUserEmailVerified(NpgsqlConnection conn, ClaimsPrincipal claimsPrincipal)
     {
-        var emailVerificationRequired = await conn.GetVerifiedEmailForPluginPublishSetting();
         var emailSettings = await emailService.GetEmailSettingsFromDb();
-        if (!emailVerificationRequired || emailSettings?.PasswordSet != true)
+        if (!await IsVerifiedEmailRequired(conn) || emailSettings?.PasswordSet != true)
             return true; // for now always return true in these cases if we don't have a verified email
 
         var user = await userManager.GetUserAsync(claimsPrincipal);
         return await userManager.IsEmailConfirmedAsync(user!);
+    }
+    
+    public async Task<bool> IsVerifiedEmailRequired(NpgsqlConnection conn)
+    {
+        var emailVerificationRequired = await conn.GetVerifiedEmailForPluginPublishSetting();
+        return emailVerificationRequired;
     }
 }
