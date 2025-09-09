ba51b23679d419c9ee8f2b67c7e32cd527a2a64d
thgO.O <thgoil@proton.me>
2025-09-09 19:25:54 -0300

    refactor: replace hardcoded keys with constants and add VerifiedEmailForLogin

--- PluginBuilder/DataModels/SettingsKeys.cs
+++ PluginBuilder/DataModels/SettingsKeys.cs
@@ -5,5 +5,6 @@ public static class SettingsKeys
 {
     public const string EmailSettings = nameof(EmailSettings);
     public const string VerifiedEmailForPluginPublish = nameof(VerifiedEmailForPluginPublish);
+    public const string VerifiedEmailForLogin = nameof(VerifiedEmailForLogin);
     public const string FirstPluginBuildReviewers = nameof(FirstPluginBuildReviewers);
 }
