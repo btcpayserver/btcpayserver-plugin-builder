7a5d0d1d8b6b59a089abc6078dad4fc056dad88f
Chukwuleta Tobechi <47084273+TChukwuleta@users.noreply.github.com>
2025-11-05 15:30:06 +0100

    Include namespace to safe file

--- PluginBuilder/Util/Safe.cs
+++ PluginBuilder/Util/Safe.cs
@@ -2,6 +2,8 @@
 using Ganss.Xss;
 using Microsoft.AspNetCore.Html;
 
+namespace PluginBuilder.Util;
+
 public static class Safe
 {
     private static readonly HtmlSanitizer _htmlSanitizer = new HtmlSanitizer();
