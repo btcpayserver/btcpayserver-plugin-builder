645b566a19c6b470848a001fed7112335f50e96b
Chukwuleta Tobechi <47084273+TChukwuleta@users.noreply.github.com>
2025-11-12 14:59:19 +0100

    review request listing

--- PluginBuilder/Util/Extensions/NpgsqlConnectionExtensions.cs
+++ PluginBuilder/Util/Extensions/NpgsqlConnectionExtensions.cs
@@ -41,7 +41,7 @@ public static class NpgsqlConnectionExtensions
 
     public static async Task<PluginViewModel?> GetPluginDetails(this NpgsqlConnection connection, PluginSlug pluginSlug)
     {
-        return await connection.QueryFirstOrDefaultAsync<PluginViewModel>("SELECT slug, identifier, settings, visibility FROM plugins WHERE slug=@pluginSlug", new { pluginSlug = pluginSlug.ToString() });
+        return await connection.QueryFirstOrDefaultAsync<PluginViewModel>("SELECT slug AS \"PluginSlug\", identifier, settings, visibility FROM plugins WHERE slug=@pluginSlug", new { pluginSlug = pluginSlug.ToString() });
     }
 
     #endregion
