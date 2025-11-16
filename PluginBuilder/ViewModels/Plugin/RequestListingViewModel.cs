5d2bf134741991382ea54d21674b5301f3a4b8bc
rockstardev <5191402+rockstardev@users.noreply.github.com>
2025-11-15 23:37:11 -0600

    Improving request listing form

--- PluginBuilder/ViewModels/Plugin/RequestListingViewModel.cs
+++ PluginBuilder/ViewModels/Plugin/RequestListingViewModel.cs
@@ -5,8 +5,6 @@ namespace PluginBuilder.ViewModels;
 public class RequestListingViewModel
 {
     public string PluginSlug { get; set; }
-
-    [Display(Name = "Release Note")]
     public string ReleaseNote { get; set; }
     public string TelegramVerificationMessage { get; set; }
     public string UserReviews { get; set; }
@@ -14,6 +12,5 @@ public class RequestListingViewModel
     public bool PendingListing { get; set; }
     public bool CanSendEmailReminder { get; set; }
 
-    [Display(Name = "Announcement Date & Time (optional)")]
     public DateTimeOffset? AnnouncementDate { get; set; }
 }
