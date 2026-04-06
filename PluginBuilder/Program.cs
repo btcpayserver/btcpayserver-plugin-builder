3399de5842b7d6587ca036e5011899b1db5005a1
Chukwuleta Tobechi <47084273+TChukwuleta@users.noreply.github.com>
2026-04-06 15:53:32 +0100

    Increase Kestrel max request body size to support image uploads

--- PluginBuilder/Program.cs
+++ PluginBuilder/Program.cs
@@ -73,6 +73,11 @@ public class Program
 
     private void ConfigureBuilder(WebApplicationBuilder builder)
     {
+        builder.WebHost.ConfigureKestrel(options =>
+        {
+            options.Limits.MaxRequestBodySize = 104857600;
+        });
+
         builder.Configuration.AddEnvironmentVariables("PB_");
 
 #if DEBUG
